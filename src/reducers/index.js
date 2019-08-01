12b6519ea30fda2b199cc68edd81e9b45cfee19a
Sam Tagert <samtagert@gmail.com>
2019-08-01 00:47:02 -0700

    setup songdetail component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,7 @@ import { combineReducers } from 'redux';
 const songsReducer = () => {
   return [
     { title: 'No Scrubs', duration: '4:05' },
-    { title: 'No Macarena', duration: '2:30' },
+    { title: 'Macarena', duration: '2:30' },
     { title: 'All Star', duration: '3:15' },
     { title: 'I Want it That Way', duration: '1:45' }
   ];
